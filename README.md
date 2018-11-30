28e9ac060a9f2648325bd38e6fe01c8c1eb5a3ea
xiaogang.zhou@qq.com <xiaogang.zhou@qq.com>
2018-11-30 17:06:08 +0800

    5. 全局签名窜验证器中间件`SignCheck`增加，自动验证接口签名窜

--- README.md
+++ README.md
@@ -3,14 +3,21 @@
 XGservice - 基于TP5的快速api构建
 =========================
 
+- [x] 目录结构调整
+    - [x] 增加常量定义文件`constant.php`
+    - [x] `application`更改为`app`使其命名空间更具语义化
+    - [x] 调整自动注册命令目录`command`到根目录，使其`app`下的目录全为模块目录
+- [x] 架构层面功能的使用示例增加：服务提供者，门面绑定，钩子回调，中间件，路由
 - [x] 自定义命令行
-    - [x] 自定义创建Model,Validate,Controller,Command
-    - [x] 更改默认中间件位置，并自定义创建middleware
+    - [x] 自定义创建`Model`,`Validate`,`Controller`,`Command`
+    - [x] 更改默认中间件位置，并自定义创建`middleware`
+    - [ ] 自定义命令`build`,一键创建模块
 - [x] 定制化redis类
     - [x] 新增`__call`,`redisLock`,`getInstance`
     - [x] 新增`CachekeyMap`静态类，批量管理缓存key
-- [x] 架构层面功能的使用示例增加：服务提供者，门面绑定，钩子回调，中间件，路由
+    - [x] 注册到app容器内
 - [x] `ResponsData`优化，以及`facade`静态调用与`traits`继承增加
 - [x] 全局参数验证器中间件`Validate`增加,自动验证不同接口的请求参数
 - [x] 全局签名窜验证器中间件`SignCheck`增加，自动验证接口签名窜
-- [ ]
+- [x] 基于`symfony/var-dumper`的`dd`,`d`打印调试函数封装
+- [ ] 定制化TP日志类
--- extend/libs/CacheKeyMap.php
+++ extend/libs/CacheKeyMap.php
@@ -4,25 +4,25 @@ namespace libs;
 
 /**
  * 缓存key映射类：缓存KEY要统一配置，便于后期批量更改和管理
- * 注意其命名规则： 项目名：模块名：名称：类型  tkmall:member:username:str
+ * 注意其命名规则： 项目名：模块名：名称：类型   tkmall:mem:uid:hash
  * Class CacheKeyMap
  * @package libs
  */
 class CacheKeyMap
 {
-    public static $prefix = 'test:';
-
+    public static $prefix = 'tkmall:';
 
     /**
-     * 战队排行数据列表
-     * @param $gameId
+     * 基于会员uid的hash
+     * @param $uid
+     * @param int $prefix
      * @return string
      */
-    public static function eventTeamRankListKey($gameId,$prefix=0)
+    public static function memberUidHash($uid,$prefix=0)
     {
         if($prefix){
-            return self::$prefix . 'event_team:rank:list:*';
+            return self::$prefix . 'mem:' . $uid .':*';
         }
-        return self::$prefix . 'event_team:rank:list:' . $gameId;
+        return self::$prefix . 'mem:' . $uid .':hash';
     }
 }

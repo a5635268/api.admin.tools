105c7a29e5751e238c194dabe949e7d0bd7ae951
zhouxiaogang <xiaogang.zhou@qq.com>
2020-05-12 17:58:34 +0800

    增加phpstorm的设置

--- tp6/app/command/Test.php
+++ tp6/app/command/Test.php
@@ -3,24 +3,15 @@
 namespace app\command;
 
 use app\common\command\BaseCommand;
-use app\common\command\create\Model;
-use app\common\facade\JWT;
 use app\common\model\WxOpenid;
 use app\common\service\Intergral;
 use app\common\service\Sms;
-use app\controller\Login;
 use app\model\Banner;
 use app\model\GoodsImagesProcess;
-use app\model\WxMember;
-use EasyWeChat\Factory;
-use libs\OssUpload;
 use think\console\Input;
 use think\console\Output;
 use think\console\input\Argument;
-use think\console\input\Option;
 use libs\Log;
-use think\facade\Db;
-use think\facade\Debug;
 
 class Test extends BaseCommand
 {
